2a285950750d5108f7c59435ba2ce2bcce3365f4
Jeff Richardson <jeff@masterpieceinteractive.com>
2016-10-05 01:17:17 -0500

    add tests for isNumeric function

--- test/calc-spec.js
+++ test/calc-spec.js
@@ -1,4 +1,4 @@
-var calc = require('../src/calc.js');
+var calc = require('../src/calc.js')();
 var assert = require('assert');
 
 /*describe('Array', function() {
@@ -13,6 +13,26 @@ describe('Calculator', function() {
    describe('#isNumeric', function() {
        it('should return false if a letter is provided', function() {
            assert.equal(false, calc.isNumeric('a'));
-       })
+       });
+
+       it('should return false if a mix characters and numbers is provided', function() {
+           assert.equal(false, calc.isNumeric('10a'));
+       });
+
+       it('should return true if a number is provided', function() {
+           assert.equal(true, calc.isNumeric(10));
+       });
+
+       it('should return true if a string with only a number is provided', function() {
+           assert.equal(true, calc.isNumeric('10'));
+       });
+
+       it('should return true if a negative number is provided', function() {
+           assert.equal(true, calc.isNumeric(-10));
+       });
+
+       it('should return true if a negative number is provided as a string', function() {
+           assert.equal(true, calc.isNumeric('-10'));
+       });
    });
 });
